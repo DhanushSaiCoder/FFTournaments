de963d54d7cc5329b64ccd2af9d6884528880331
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-05-05 15:57:18 +0530

    Refactor code structure for improved readability and maintainability

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,32 +1,37 @@
 import React from 'react';
 import { Link, useLocation } from 'react-router-dom';
 import '../styles/Header.css'; // Optional: Add styles for active links
-
+import logo from '../images/Logo.png'; // Import your logo image
 const Header = () => {
   const location = useLocation(); // Get the current location
 
   return (
-    <header>
-      <nav>
-        <ul>
-          <li className={location.pathname === '/' ? 'active' : ''}>
-            <Link to="/">Home</Link>
-          </li>
-          <li className={location.pathname === '/tournaments' ? 'active' : ''}>
-            <Link to="/tournaments">Tournaments</Link>
-          </li>
-          <li className={location.pathname === '/rankings' ? 'active' : ''}>
-            <Link to="/rankings">Rankings</Link>
-          </li>
-          <li className={location.pathname === '/help' ? 'active' : ''}>
-            <Link to="/help">Help</Link>
-          </li>
-          <li className={location.pathname === '/contactUs' ? 'active' : ''}>
-            <Link to="/contactUs">Contact Us</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <div className="header">
+      <div className='logoDiv'></div>
+
+      <div className='navLinksDiv'>
+        <nav>
+          <ul>
+            <li className={location.pathname === '/' ? 'active' : ''}>
+              <Link to="/">HOME</Link>
+            </li>
+            <li className={location.pathname === '/tournaments' ? 'active' : ''}>
+              <Link to="/tournaments">TOURNAMENTS</Link>
+            </li>
+            <li className={location.pathname === '/rankings' ? 'active' : ''}>
+              <Link to="/rankings">RANKING</Link>
+            </li>
+            <li className={location.pathname === '/help' ? 'active' : ''}>
+              <Link to="/help">HELP</Link>
+            </li>
+            <li className={location.pathname === '/contactUs' ? 'active' : ''}>
+              <Link to="/contactUs">CONTACT US</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+    </div >
   );
 };
 
