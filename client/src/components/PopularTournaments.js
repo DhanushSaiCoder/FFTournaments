a2d5ca5cb94808a5ad680165db451ac959700971
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-05-05 17:51:11 +0530

    Refactor Home component to use HeroSection and PopularTournaments components; remove legacy HomeHeroDiv structure and styles

---
+++ client/src/components/PopularTournaments.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import "../styles/PopularTournaments.css";
+
+const PopularTournaments = () => {
+    return (
+        <div>
+            <h2>Popular Tournaments</h2>
+            <div className="tournament-list">
+                <div className="tournament-item">
+                    <h3>Tournament 1</h3>
+                    <p>Date: 2023-10-01</p>
+                    <p>Prize Pool: $1000</p>
+                </div>
+                <div className="tournament-item">
+                    <h3>Tournament 2</h3>
+                    <p>Date: 2023-10-15</p>
+                    <p>Prize Pool: $2000</p>
+                </div>
+                <div className="tournament-item">
+                    <h3>Tournament 3</h3>
+                    <p>Date: 2023-11-01</p>
+                    <p>Prize Pool: $1500</p>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default PopularTournaments;
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import "../styles/Home.css";
-
+import HeroSection from '../components/HeroSection';
+import PopularTournaments from '../components/PopularTournaments';
 const Home = () => {
     return (
         <div className='Home'>
-            <div className='HomeHeroDiv'>
-                <div className='HomeHeroContent'>
-                    <div className='heroLeft'>
-                        <h1>Win Exciting Cash Prizes<br/>By Playing Your Favorite Game.</h1>
-                        <p>Join college-level Free Fire tournaments<br/>and win exciting cash prizes!</p>
-                        <div className='heroCTADiv'>
-                            <button className='registerBtn'>Register</button>
-                            <button className='loginBtn'>Log in</button>
-                        </div>
-                    </div>
-                    <div className='heroRight'></div>
-                    
-                </div>
-            </div>
+           <HeroSection />
+           <PopularTournaments />
         </div>
     );
 }
