a264d79a969b760ea23e4f0337a90635c9c06b97
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-05-06 18:56:09 +0530

    Refactor BackButton component structure and enhance CSS styles for improved layout and hover effects

--- client/src/components/BackButton.js
+++ client/src/components/BackButton.js
@@ -4,10 +4,12 @@ import "../styles/BackButton.css"; // Import your CSS file for styling
 const BackButton = () => {
     const navigate = useNavigate();
     return (
-        <div onClick={() => {
-            navigate(-1); // Go back to the previous page
-        }} className='backButtonContainer'>
-            &lt; Back
+        <div className='backButtonContainer'>
+            <div onClick={() => {
+                navigate(-1); // Go back to the previous page
+            }} className='backButton'>
+                &lt; Back
+            </div>
         </div>
     );
 }
