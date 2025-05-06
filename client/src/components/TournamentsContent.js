26d6810165a2ff12a0ee0b1e21526469bb89edfd
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-05-06 15:57:14 +0530

    Add TournamentsContent component and integrate with Tournaments page; update styles and layout

---
+++ client/src/components/TournamentsContent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../styles/TournamentsContent.css"
+const TournamentsContent = () => {
+    return (
+        <div className='tournamentsContent'>
+            <h2>Tournaments Content</h2>
+            <p>This is the tournaments content section.</p>
+        </div>
+    );
+}
+
+export default TournamentsContent;
--- client/src/pages/Tournaments.js
+++ client/src/pages/Tournaments.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import "../styles/Tournaments.css";
 import TournamentsHeroSection from '../components/TournamentsHeroSection';
+import SectionDivider from './../components/SectionDivider';
+import TournametsContent from '../components/TournamentsContent';
+
 const Tournaments = () => {
     return (
         <div className='Tournaments'>
             <TournamentsHeroSection />
+            <SectionDivider text="ALL TOURNAMENTS" />
+            <TournametsContent />
         </div>
     );
 }
