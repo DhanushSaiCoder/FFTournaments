6a9f10e4478df2c9db592aa5e5b36adf96adac5e
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-05-06 14:29:29 +0530

    Refactor TournamentsHeroSection component layout and styles for improved user interface

--- client/src/components/TournamentsHeroSection.js
+++ client/src/components/TournamentsHeroSection.js
@@ -4,16 +4,21 @@ import "../styles/TournamentsHeroSection.css";
 const TournamentsHeroSection = () => {
     return (
         <div className="tournamentsHeroSection">
-            <h1>Tournaments</h1>
-            <p>Join our tournaments and compete with players from around the world!</p>
-            <button className="btn btn-primary">Join Now</button>
-            <div className="tournament-list">
-                <h2>Upcoming Tournaments</h2>
-                <ul>
-                    <li>Tournament 1 - Date: 2023-10-01</li>
-                    <li>Tournament 2 - Date: 2023-10-15</li>
-                    <li>Tournament 3 - Date: 2023-11-01 </li>
-                </ul>
+            <div className='tsHeroSectionContent'>
+                <div className='tsHeroSectionLeft'>
+                    <div className='userCostumeImgDiv'>
+                        {/* image with background  with css. */}
+                    </div>
+                    <div className='userUserNameDiv'>
+                        <div className='tsHeroUserName'>
+                            <p>꧁༒☬𝕵𝖆𝖌𝖚𝖆𝖗☬༒꧂</p>
+                        </div>
+                        <div className='tsHeroUserId'>
+                            <p>UID: 8919552348</p>
+                        </div>
+                    </div>
+                </div>
+                <div className='tsHeroSectionRight'></div>
             </div>
         </div>
     );
