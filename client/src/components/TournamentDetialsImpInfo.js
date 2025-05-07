294ed5336ba21181e0db16e8de5cca660638e08e
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-05-07 14:30:33 +0530

    Add TournamentDetailsContent and TournamentDetialsImpInfo components with sample data and structure

---
+++ client/src/components/TournamentDetialsImpInfo.js
@@ -0,0 +1,37 @@
+import React from 'react';
+
+const TournamentDetialsImpInfo = () => {
+    return (
+        <div>
+            <h2>Important Information</h2>
+            <div className="importantInformation">
+                <h3>Details</h3>
+                <ul>
+                    <li>Important information about the tournament</li>
+                    <li>More important information about the tournament</li>
+                    <li>Even more important information about the tournament</li>
+                </ul>
+                <h3>Rules</h3>
+                <ul>
+                    <li>Rule 1</li>
+                    <li>Rule 2</li>
+                    <li>Rule 3</li>
+                </ul>
+                <h3>How to Join</h3>
+                <ul>
+                    <li>How to join the tournament</li>
+                    <li>More information on how to join</li>
+                    <li>Even more information on how to join</li>
+                </ul>
+                <h3>How to Claim Prize Money</h3>
+                <ul>
+                    <li>How to claim the prize money</li>
+                    <li>More information on how to claim the prize money</li>
+                    <li>Even more information on how to claim the prize money</li>
+                </ul>
+            </div>
+        </div>
+    );
+}
+
+export default TournamentDetialsImpInfo;
