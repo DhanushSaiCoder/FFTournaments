67f64ccb59deb18a681c993f392ccf77b3467630
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-05-16 15:06:00 +0530

    feat: Add RegistrationStepper component and update RegisterPage
    - Implemented RegistrationStepper component with custom styling and icons using MUI.
    - Updated RegisterPage to include RegistrationStepper and improved layout.
    - Added new dependencies for MUI components and Emotion styling.
    - Enhanced styles for RegisterPage and Footer components for better UI.

--- client/src/pages/RegisterPage.js
+++ client/src/pages/RegisterPage.js
@@ -1,16 +1,19 @@
 import React from 'react';
+import "../styles/RegisterPage.css"
+import RegistrationStepper from '../components/RegistrationStepper';
+
+
 
 const RegisterPage = () => {
-    return (
-        <div>
-            <h2>Hello eorkd</h2>
-            <h2>Hello eorkd</h2>
-            <h2>Hello eorkd</h2>
-            <h2>Hello eorkd</h2>
-            <h2>Hello eorkd</h2>
-            <h2>Hello eorkd</h2>
-        </div>
-    );
+
+  return (
+    <div className='RegisterPageContainer'>
+      <div className='RegisterPageContent'>
+        <h2>REGISTER FOR TOURNAMENT</h2>
+        <RegistrationStepper />
+      </div>
+    </div>
+  );
 }
 
 export default RegisterPage;
