729206891edef88346000617ad255d1fd8d26d14
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-05-16 14:30:12 +0530

    Add registration functionality and create RegisterPage component

--- client/src/index.js
+++ client/src/index.js
@@ -14,6 +14,7 @@ import Profile from './pages/Profile';
 import Settings from './pages/Settings';
 import Footer from './components/Footer';
 import TournamentDetails from './components/TournamentDetails';
+import RegisterPage from './pages/RegisterPage';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -24,6 +25,7 @@ root.render(
         <Route path="/" element={<Home />} />
         <Route path="/tournaments" element={<Tournaments />} />
         <Route path="/tournament/:id" element={<TournamentDetails />} />
+        <Route path="/register/:id" element={<RegisterPage />} />
         <Route path="/rankings" element={<Rankings />} />
         <Route path="/help" element={<Help />} />
         <Route path="/contactUs" element={<ContactUs />} />
---
+++ client/src/pages/RegisterPage.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const RegisterPage = () => {
+    return (
+        <div>
+            <h2>Hello eorkd</h2>
+            <h2>Hello eorkd</h2>
+            <h2>Hello eorkd</h2>
+            <h2>Hello eorkd</h2>
+            <h2>Hello eorkd</h2>
+            <h2>Hello eorkd</h2>
+        </div>
+    );
+}
+
+export default RegisterPage;
