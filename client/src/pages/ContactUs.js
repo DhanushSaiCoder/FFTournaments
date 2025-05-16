27db220925abe140158a470ac205e0dba13d9c85
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-05-16 21:49:37 +0530

    feat: Add ContactUs component with styling for improved layout and user experience

--- client/src/pages/ContactUs.js
+++ client/src/pages/ContactUs.js
@@ -1,26 +1,30 @@
 import React from 'react';
+import "../styles/ContactUs.css";
 
 const ContactUs = () => {
     return (
-        <div>
-            <h1>Contact Us</h1>
-            <p>If you have any questions or feedback, please reach out to us!</p>
-            <form>
-                <div>
-                    <label htmlFor="name">Name:</label>
-                    <input type="text" id="name" name="name" required />
-                </div>
-                <div>
-                    <label htmlFor="email">Email:</label>
-                    <input type="email" id="email" name="email" required />
-                </div>
-                <div>
-                    <label htmlFor="message">Message:</label>
-                    <textarea id="message" name="message" required></textarea>
-                </div>
-                <button type="submit">Send</button>
-            </form>
+        <div className='ContactUsContainer'>
+            <div className='ContactUs'>
+                <h1>Contact Us</h1>
+                <p>If you have any questions or feedback, please reach out to us!</p>
+                <form>
+                    <div>
+                        <label htmlFor="name">Name:</label>
+                        <input type="text" id="name" name="name" required />
+                    </div>
+                    <div>
+                        <label htmlFor="email">Email:</label>
+                        <input type="email" id="email" name="email" required />
+                    </div>
+                    <div>
+                        <label htmlFor="message">Message:</label>
+                        <textarea id="message" name="message" required></textarea>
+                    </div>
+                    <button type="submit">Send</button>
+                </form>
+            </div>
         </div>
+
     );
 }
 
