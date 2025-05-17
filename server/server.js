c7dd5999083f32afc1b2d2eabc9f9c8751d507a0
DhanushSaiCoder <dhanushsai1467@gmail.com>
2025-05-17 13:22:41 +0530

    feat: Update server configuration to use environment variables for port and MongoDB connection

--- server/server.js
+++ server/server.js
@@ -3,10 +3,14 @@ const app = express()
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-const port = 5000
+const port = process.env.PORT
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('MongoDB connected'))
+    .catch((err) => console.error('MongoDB connection error:', err));
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('<h1>Hello, Gamer!</h1><p>So, you are a gamer with some tech skills right!</p>')
 })
 
 app.listen(port, () => {
@@ -14,6 +18,3 @@ app.listen(port, () => {
 })
 
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('MongoDB connected'))
-    .catch((err) => console.error('MongoDB connection error:', err));
